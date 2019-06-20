2ebf17b45640924cf41c1e32924c23d5f3d39b61
Josh Nho <joshnho@gmail.com>
2019-06-20 00:13:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,4 +2,13 @@ def using_push(colors_in_the_rainbow, next_color)
   updated_array = colors_in_the_rainbow.push(next_color)
 end
 
+def using_unshift(bouroughs_in_nyc)
 
+
+
+describe "using_unshift" do 
+  before(:each) do 
+    bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+    @new_neighborhood = "Staten Island"
+    @updated_array = using_unshift(bouroughs_in_nyc, @new_neighborhood)
+  end
