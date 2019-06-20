d4525df5f7bab5bc66fe1f6da681cfedb6d87ec4
Josh Nho <joshnho@gmail.com>
2019-06-20 00:08:47 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,5 +2,5 @@ def using_push(color_in_the_rainbow, next_color)
   colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
   next_color = "violet"
   all_colors = colors_in_the_rainbow.push
-  expect(updated_array.last).to eq(next_color)
+  all_colors[-1] = next_color
 end
